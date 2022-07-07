886b35426e79afc8345b6609b4fd60d86e037f2f
duongphung565 <nguyenphungduong565@gmail.com>
2022-07-07 21:00:32 +0700

    lab04

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+function RenderDish({ dish }) {
+    console.log("renderDish");
+}
+function RenderComments({ comments }) {
+    console.log("RenderComments");
+}
+const DishDetail = (props) => {
+    console.log("Dish Detail");
+}
+
+export default DishDetail;
